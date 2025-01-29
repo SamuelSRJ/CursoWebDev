5be9c379078c008c7dd7e05b41810b687d98debb
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-01-29 20:45:16 -0300

    Commit - Modulo MVC

---
+++ Modulo_MVC/projetos/miniframework/App/Controllers/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+    namespace App\Controllers;
+
+    use MF\Controller\Action;
+    use App\Connection;
+    use App\Models\Produto;
+
+    class IndexController extends Action{
+
+        public function index() {
+            // $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+
+            // Instância de conexão
+            $conn = Connection::getDb();
+
+            // Instância de modelo
+            $produto = new Produto($conn);
+            $produtos = $produto->getProdutos();
+            $this->view->dados = $produtos;
+
+            $this->render('index', 'layout1');
+        }
+
+        public function sobreNos() {
+            // $this->view->dados = array('Notebook', 'Smartphone');
+            $this->render('sobreNos', 'layout1');
+        }
+
+    }
+
+?>
